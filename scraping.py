68c14565e62007c2bf3473fa2f17d767803baac9
Wilson Alexei <wilsonalexei123@yahoo.com>
2022-02-22 17:30:11 -0500

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -1,25 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[12]:
-
-
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
-
-# In[6]:
-
-
+#Set Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
-
-
-# In[10]:
-
 
 # Visit the mars nasa news site
 url = 'https://redplanetscience.com'
@@ -27,82 +14,40 @@ browser.visit(url)
 # Optional delay for loading the page
 browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-
-# In[11]:
-
-
 #Set up the HTML Parser
 html = browser.html
 news_soup = soup(html, 'html.parser')
 slide_elem = news_soup.select_one('div.list_text')
-
-
-# In[12]:
-
-
 slide_elem.find('div', class_='content_title')
-
-
-# In[13]:
-
 
 # Use the parent element to find the first `a` tag and save it as `news_title`
 news_title = slide_elem.find('div', class_='content_title').get_text()
 news_title
 
-
-# In[14]:
-
-
 # Use the parent element to find the paragraph text
 news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 news_p
 
-
 # ### Featured Images
-
-# In[7]:
-
-
 # Visit URL
 url = 'https://spaceimages-mars.com'
 browser.visit(url)
-
-
-# In[8]:
-
 
 # Find and click the full image button
 full_image_elem = browser.find_by_tag('button')[1]
 full_image_elem.click()
 
-
-# In[9]:
-
-
 # Parse the resulting html with soup
 html = browser.html
 img_soup = soup(html, 'html.parser')
-
-
-# In[10]:
-
 
 # Find the relative image url
 img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 img_url_rel
 
-
-# In[11]:
-
-
 # Use the base URL to create an absolute URL
 img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 img_url
-
-
-# In[15]:
-
 
 #Read the HTML table and transform into a DataFrama with Pandas
 df = pd.read_html('https://galaxyfacts-mars.com')[0]
@@ -110,21 +55,10 @@ df.columns=['description', 'Mars', 'Earth']
 df.set_index('description', inplace=True)
 df
 
-
-# In[16]:
-
-
 #Convert DataFrame back into HTML-ready code
 df.to_html()
 
-
-# In[17]:
-
-
 browser.quit()
-
-
-# In[ ]:
 
 
 
