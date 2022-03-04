64ff8a2ad000fc1b8e2916d6fd8e341bf217fa9f
Wilson Alexei <wilsonalexei123@yahoo.com>
2022-03-04 16:16:19 -0500

    Deliverable 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template #, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
@@ -17,8 +17,8 @@ def index():
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
-   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   mars.update({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
